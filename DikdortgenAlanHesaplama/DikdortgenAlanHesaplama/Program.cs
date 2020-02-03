2f55bfaf7cfbe59896e29643c0b175b4d89b2ac6
omer kor <omerkorr@gmail.com>
2020-02-03 11:12:36 +0300

    ekleme

--- DikdortgenAlanHesaplama/DikdortgenAlanHesaplama/Program.cs
+++ DikdortgenAlanHesaplama/DikdortgenAlanHesaplama/Program.cs
@@ -10,6 +10,27 @@ namespace DikdortgenAlanHesaplama
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("kısa kenarın uzunluğunu giriniz");
+            double kk = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("uzun kenarın uzunluğunu giriniz");
+            double uk = Convert.ToDouble(Console.ReadLine());
+            double dikdortgenalan = alanvecevrehesaplama.alanhesapla(kk, uk);
+            double dikdortgencevre = alanvecevrehesaplama.cevrehesapla(kk, uk);
+            Console.Write("Dikdörtgenin alanı: {0}  Dikdörtgenin çevresi: {1}", dikdortgenalan, dikdortgencevre);
+            Console.ReadKey();
+        }
+        class alanvecevrehesaplama
+        {
+            public static double alanhesapla(double kisakenar, double uzunkenar)
+            {
+                double alan = kisakenar * uzunkenar;
+                return (alan);
+            }
+            public static double cevrehesapla(double kisakenar, double uzunkenar)
+            {
+                double cevre = 2 * (kisakenar + uzunkenar);
+                return (cevre);
+            }
         }
     }
 }
